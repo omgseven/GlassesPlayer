e7dd9faa3a2a0b5e4dbbcdd77dc55671fb4ff974
chaoyi <xuechaoyi.xcy@alibaba-inc.com>
2026-06-03 20:03:26 +0800

    init

---
+++ GlassesPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GlassesPlayer
+//
+//  Created by chaoyi on 2026/6/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GlassesPlayer/GlassesPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassesPlayerApp.swift
+//  GlassesPlayer
+//
+//  Created by chaoyi on 2026/6/3.
+//
+
+import SwiftUI
+
+@main
+struct GlassesPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
